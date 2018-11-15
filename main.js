577a86108d061dc10314b25a5a324e6739e80111
Gaya Kessler <gaya.kessler@gmail.com>
2018-11-15 16:56:29 +0100

    write connection layer

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
-(function(w) {
-  // component > register
-  // epic > inject
-  // reducer > register (strip out store for this one)
-  // settings > register
-  // table > registerColumn(s)
-})(window);
+function callMethod(name) {
+  return function() {
+    if (window.thyme[name]) {
+      window.thyme[name](arguments);
+    }
+
+    if (!window.thymeConnect[name]) window.thymeConnect[name] = [];
+    window.thymeConnect[name].push(arguments);
+  }
+}
+
+function createMethod(name, func) {
+  window.thyme[name] = func;
+
+  if (window.thymeConnect[name]) {
+    window.thymeConnect[name].forEach(args => func.apply(null, args));
+  }
+}
+
+module.exports = {
+  registerComponent: callMethod('registerComponent'),
+  injectEpic: callMethod('injectEpic'),
+  registerReducer: callMethod('registerReducer'),
+  registerSettingsPanel: callMethod('registerSettingsPanel'),
+  registerColumn: callMethod('registerColumn'),
+  registerColumns: callMethod('registerColumns'),
+  invoke: createMethod,
+};
+
