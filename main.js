22c0da33a52d490a304df6f994926f6e7d40ceba
Gaya Kessler <gaya.kessler@gmail.com>
2018-11-15 16:23:39 +0100

    add skeleton module

---
+++ main.js
@@ -0,0 +1,7 @@
+(function(w) {
+  // component > register
+  // epic > inject
+  // reducer > register (strip out store for this one)
+  // settings > register
+  // table > registerColumn(s)
+})(window);
